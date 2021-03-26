4e6a697261bef41c04857d1b1630aeee4df90196
Igor Emerick <igorbarbosaemerick@gmail.com>
2021-03-26 20:36:14 -0300

    Adicionando saque.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,95 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const port = 3030;
+const {v4} = require('uuid');
 
 app.use(express.json());
 
-app.get('/courses', (req, res) => {
-  const query = req.query;
-  console.log(query);
-  return res.json(['Curso 1', 'Curso 2', 'Curso 3']);
+const customers = [];
+
+function verifyIfExistAccountCpf(req, res, next) {
+  const {cpf} = req.headers;
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(400).json({error: 'Customer not found!'});
+  }
+
+  req.customer = customer;
+
+  return next();
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
+app.post('/account', (req, res) => {
+  const { cpf, name } = req.body;
+  const customerAlreadyExist = customers.some(customer => customer.cpf === cpf);
+
+  if (customerAlreadyExist) {
+    return res.status(400).json({error: "Customer already exists!"});
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: v4(),
+    statement: []
+  });
+
+  return res.status(201).send();
 });
 
-app.post('/courses', (req, res) => {
-  const body = req.body;
-  console.log(body);
-  return res.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4']);
+app.get('/statement/', verifyIfExistAccountCpf, (req, res) => {
+  const {customer} = req;
+  
+  return res.json(customer.statement);
 });
 
-app.put('/courses/:id', (req, res) => {
-  const {id} = req.params;
-  console.log(id);
-  return res.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4']);
-});
+app.post('/deposit', verifyIfExistAccountCpf, (req, res) => {
+  const {description, amount} = req.body;
+  const {customer} = req;
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit'
+  }
 
-app.patch('/courses/:id', (req, res) => {
-  return res.json(['Curso 6', 'Curso 7', 'Curso 3', 'Curso 4']);
-});
+  customer.statement.push(statementOperation);
 
-app.delete('/courses/:id', (req, res) => {
-  return res.json(['Curso 6', 'Curso 2', 'Curso 4']);
-});
+  return res.status(201).send();
+})
+
+app.post('/withdraw', verifyIfExistAccountCpf, (req, res) => {
+  const { amount, description } = req.body;
+  const { customer } = req;
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return res.status(400).json({error: 'Insufficient funds!'});
+  }
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'debit'
+  }
+  // customer.statement.push(statementOperation);
+
+  return res.status(201).send();
+})
 
 app.listen(port);
